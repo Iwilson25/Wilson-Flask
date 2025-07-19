1b36c712c7d2e52bf9e5940610b5c266c9c1cce5
iWilson25 <wilsonlyx250294@gmail.com>
2025-07-19 11:57:27 +0800

    For EC2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Wilson!</p>"
+    return "<p>Hello, WilsonEC2!</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
